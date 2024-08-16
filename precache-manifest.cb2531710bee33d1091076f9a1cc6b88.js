13f2fa7a567422d52a44a52fcbe69eebadd70d45
yenslife <77geo5rge6@gmail.com>
2024-08-16 13:36:26 +0800

    Updates

---
+++ precache-manifest.cb2531710bee33d1091076f9a1cc6b88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba38f55724f55c3d7eeef91a029732ea",
+    "url": "/ncku-dorm-committee-intro-2024/index.html"
+  },
+  {
+    "revision": "f61f2ce583ce577a571a",
+    "url": "/ncku-dorm-committee-intro-2024/static/css/main.1624d5f1.chunk.css"
+  },
+  {
+    "revision": "ea53ec919d6cf2dfc7be",
+    "url": "/ncku-dorm-committee-intro-2024/static/js/2.bd03a770.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/ncku-dorm-committee-intro-2024/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "f61f2ce583ce577a571a",
+    "url": "/ncku-dorm-committee-intro-2024/static/js/main.5887ef81.chunk.js"
+  },
+  {
+    "revision": "4049389684facfa27eac",
+    "url": "/ncku-dorm-committee-intro-2024/static/js/runtime~main.100a5cb4.js"
+  }
+]);
