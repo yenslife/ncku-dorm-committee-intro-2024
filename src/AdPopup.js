3a7a6f08fd011673db4511fe0929a66701e882b5
yenslife <77geo5rge6@gmail.com>
2024-08-25 00:57:14 +0800

    Enhance AdPopup component with accessibility improvements and visual styling
    - Wrapped emojis in <span> with role="img" and accessible descriptions using aria-label for better accessibility.
    - Added borders and box-shadows to popup for improved visual appearance.
    - Increased social icon size and spacing for better user interaction.
    - Added new content copy for clarity and engagement.
    - Minor text adjustments for clarity and consistency.

--- src/AdPopup.js
+++ src/AdPopup.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './AdPopup.css'; // 引入CSS以便於樣式設置
+import { BsFacebook, BsInstagram } from "react-icons/bs";
 
 const AdPopup = ({ isOpen, onClose }) => {
     if (!isOpen) return null; // 如果沒有打開，則不渲染任何內容
@@ -8,8 +9,26 @@ const AdPopup = ({ isOpen, onClose }) => {
         <div className="popup-overlay">
             <div className="popup-content">
                 <button className="close-button" onClick={onClose}>✖</button> {/* 關閉按鈕 */}
-                <h2>按讚標題</h2>
-                <p>這是你的按讚內容！...(等確認文案再加上去)</p>
+                <h2>
+                    <span role="img" aria-label="廣播">📢</span> 活動快報 <span role="img" aria-label="廣播">📢</span>
+                </h2>
+                <p>就是這兩天~~<span role="img" aria-label="日期">8/30、9/1</span></p>
+                <p style={{ fontWeight: 'bold', color: '#ff5722' }}>
+                    <span role="img" aria-label="向下箭頭">👇</span> 快追蹤宿委會 FB or IG <span role="img" aria-label="向下箭頭">👇</span>
+                </p>
+                <a href="https://www.facebook.com/DormCommittee.NCKU" className="social-icon">
+                    <BsFacebook style={{ color: '#3b5998'}} />
+                </a>
+                <a href="https://www.instagram.com/dormcommittee_ncku/" className="social-icon">
+                    <BsInstagram style={{ color: '#E1306C'}} />
+                </a>
+                <p>不僅能知道我們最近要辦的活動</p>
+                <p>
+                    現在到宿委會攤位還能獲得
+                    <b>飲料</b><span role="img" aria-label="果汁杯">🧃</span>
+                </p>
+                <p>甚至有意想不到的驚喜</p>
+                <p>就等你來啦！</p>
             </div>
         </div>
     );
